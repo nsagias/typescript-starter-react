294097bd7e26ed5a010ea3f465f44fcd85f5a10a
Nick <nsagias@gmail.com>
2021-12-20 13:33:39 -0500

    change font to h2 and change fragment to ul

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -3,12 +3,12 @@ import React from "react";
 const TodoList: React.FC = () => {
   const todoList = [{id:'t1', text: 'Do Laundry!'}];
   const pasrsedtodos = Array.isArray(todoList) 
-    && todoList.map(todo =>  <li key={todo.id}>{todo.text}</li>)
+    && todoList.map(todo => <h2> <li key={todo.id}>{todo.text}</li></h2>);
   
   return (
-    <>
+    <ul>
     {pasrsedtodos}
-    </>
+    </ul>
   );
 };
 
