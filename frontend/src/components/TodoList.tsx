674400c7c552c5e25bc1b675a907f6942523a630
Nick <nsagias@gmail.com>
2021-12-20 13:28:54 -0500

    parsed todolist into each elements

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const TodoList: React.FC = () => {
+  const todoList = [{id:'t1', text: 'Do Laundry!'}];
+  const pasrsedtodos = Array.isArray(todoList) 
+    && todoList.map(todo =>  <li key={todo.id}>{todo.text}</li>)
+  
   return (
-    <></>
+    <>
+    {pasrsedtodos}
+    </>
   );
 };
 
