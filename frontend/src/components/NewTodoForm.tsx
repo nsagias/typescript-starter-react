c323c6d93347a463a5882e38ec48d96ee4463161
Nick <nsagias@gmail.com>
2021-12-20 14:15:04 -0500

    Add form code to componenent

--- frontend/src/components/NewTodoForm.tsx
+++ frontend/src/components/NewTodoForm.tsx
@@ -2,7 +2,13 @@ import React from "react";
 
 const NewTodoForm: React.FC = () => {
   return (
-    <></>
+    <form>
+      <div>
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" />
+      </div>
+      <button type="submit">Add Todo</button>
+    </form>
   );
 };
 
