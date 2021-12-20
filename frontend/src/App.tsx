8f6556c22d4d217f5566a5e58aabd9b2f43917d5
Nick <nsagias@gmail.com>
2021-12-20 14:11:32 -0500

    Import NewTodoForm into App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
 
 function App() {
   const todoList = [{id:'t1', text: 'Do Laundry!'}];
   return (
     <div className="App">
+      <NewTodoForm />
       <TodoList items={todoList}/>
     </div>
   );
