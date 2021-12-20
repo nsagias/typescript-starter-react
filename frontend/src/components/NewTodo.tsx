a71e934dccb33fbd824f122063bbf01aeb277145
Nick <nsagias@gmail.com>
2021-12-20 14:10:49 -0500

    Rename component to NewTodoForm

--- frontend/src/components/NewTodo.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NewTodo: React.FC = () => {
-  return (
-    <></>
-  );
-};
-
-export default NewTodo;
---
+++ frontend/src/components/NewTodoForm.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewTodoForm: React.FC = () => {
+  return (
+    <></>
+  );
+};
+
+export default NewTodoForm;
