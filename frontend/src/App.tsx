0f2b49346dfc68d2aef279e71d46735ea8a4be10
Nick <nsagias@gmail.com>
2021-12-20 15:27:50 -0500

    Create addHandler fucntions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,12 @@ import TodoList from './components/TodoList';
 
 function App() {
   const todoList = [{id:'t1', text: 'Do Laundry!'}];
+
+  const todoAddHandler = (text: string) => {
+    console.log('App Received Text: ', text)
+  };
+;
+
   return (
     <div className="App">
       <NewTodoForm />
