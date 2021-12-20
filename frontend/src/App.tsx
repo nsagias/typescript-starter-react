9e180cfe03d788a444a108311d7c37ecf7c63bd6
Nick <nsagias@gmail.com>
2021-12-20 16:15:46 -0500

    install uuid and its types

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import NewTodoForm from './components/NewTodoForm';
 import TodoList from './components/TodoList';
+import uuid from "uuid";
 
 const App: React.FC = () => {
-  const todoList = useState([]);
+  const [todos, setTodos] = useState([]);
 
   const todoAddHandler = (text: string) => {
     console.log('App Received Text: ', text);
+    setTodos([{id: uuid.v4()}])
   };
 
   return (
     <div className="App">
       <NewTodoForm onAddTodo={todoAddHandler} />
-      <TodoList items={todoList} />
+      <TodoList items={todos} />
     </div>
   );
 }
